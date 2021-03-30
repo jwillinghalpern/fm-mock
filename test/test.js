e858fe684ba434fe62a7e1ecedacdac985ba679f
Joshua Willing <jwillinghalpern@gmail.com>
2021-03-29 21:52:09 -0700

    script names now case-insensitive

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ import { assert } from 'chai';
 import { mockScript } from '../src/fm-mock';
 
 const scriptName = 'My Script';
+const scriptNameLowerCase = scriptName.toLowerCase();
 const fn = () => 'hello world';
 mockScript('My Script', fn);
 
@@ -23,12 +24,13 @@ describe('mock window.FileMaker', () => {
 describe('mockScript', () => {
   it('should add a script to window.FileMaker.mockedScripts', () => {
     assert.strictEqual(
-      window.FileMaker.mockedScripts[scriptName],
+      window.FileMaker.mockedScripts[scriptNameLowerCase],
       fn,
       "function didn't match"
     );
     assert.strictEqual(
-      window.FileMaker.mockedScripts[scriptName](),
+      // fm-mock lowercases script names to make case-insensitive
+      window.FileMaker.mockedScripts[scriptNameLowerCase](),
       fn(),
       "fn output didn't match"
     );
