4a3950d46e892bb797ff7689d42ba180f1473216
Joshua Willing <jwillinghalpern@gmail.com>
2021-04-09 16:47:52 -0700

    update d.ts

--- dist/fm-mock.d.ts
+++ dist/fm-mock.d.ts
@@ -1,21 +1,11 @@
 /**
- * replacement for FileMaker.PerformScriptWithOption
- */
-declare function performScriptWithOption(script: string, param: string, option: number): void;
-
-/**
- * replacement for FileMaker.PerformScript
- */
-declare function performScript(script: string, param: string): void;
-
-/**
  * register a FM script name and the function to call instead when that script
  * is called via FileMaker.PerformScript
  * @param scriptName - FM script name
  * @param functionToCall - JS function to call instead
  * @param functionToCall.param - param you'd pass to FileMaker
  */
-declare function mockScript(scriptName: string, functionToCall: {
+export declare function mockScript(scriptName: string, functionToCall: {
     param: string;
 }): void;
 
